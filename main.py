a36d88663a51b015e7f3fb0b4b685045651bb192
Inner-shibora <mrnong.bnt3@gmail.com>
2025-05-24 21:06:29 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,32 @@
 import os
-import requests
+import telebot
 from flask import Flask, request
 
+API_TOKEN = os.getenv("TELEGRAM_TOKEN")
+bot = telebot.TeleBot(API_TOKEN)
+
 app = Flask(__name__)
-TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
-TELEGRAM_API_URL = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
 
+# ตอบกลับเมื่อมีข้อความใหม่
+@bot.message_handler(func=lambda message: True)
+def echo_message(message):
+    bot.reply_to(message, f"คุณพิมพ์ว่า: {message.text}")
+
+# route สำหรับ webhook
+@app.route(f"/bot{API_TOKEN}", methods=['POST'])
+def getMessage():
+    json_str = request.get_data().decode('UTF-8')
+    update = telebot.types.Update.de_json(json_str)
+    bot.process_new_updates([update])
+    return "!", 200
+
+# root test
 @app.route("/")
-def index():
-    return "Bot is alive!"
-
-@app.route("/webhook", methods=["POST"])
-def webhook():
-    data = request.get_json()
-    chat_id = data["message"]["chat"]["id"]
-    text = data["message"]["text"]
-
-    payload = {
-        "chat_id": chat_id,
-        "text": f"คุณพิมพ์ว่า: {text}"
-    }
-    requests.post(TELEGRAM_API_URL, json=payload)
-
-    return "OK"
+def root():
+    return "Echo Bot is live!"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    bot.remove_webhook()
+    bot.set_webhook(url=f"https://ibora-echo-bot-production.up.railway.app/bot{API_TOKEN}")
+    app.run(host="0.0.0.0", port=port)
